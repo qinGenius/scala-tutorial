9b648c7d428eb1794e683878d915708331a83c1a
qinlinsen <179880760@qq.com>
2019-11-03 21:20:42 +0800

    Rational class and it's test    ！

--- src/com/eagle/functionalobjects/Rational.scala
+++ src/com/eagle/functionalobjects/Rational.scala
@@ -1,16 +1,53 @@
 package com.eagle.functionalobjects
 
 /**
+  *scala中的构造方法有primary constructor 和auxiliary constructor 两种
+  * 这里的分数假设都是非负数
   * 在scala中类可以带参数，类中带的参数scala编译器会自动生成一个带同样参数的构造方法
   * @param numerator
   * @param denominator
   */
 class Rational(numerator:Int,denominator:Int) {
   //  println(s"numerator=${numerator}/n denominator= ${denominator}")
+  //add fields
+  val g=greatestCommonDivisor(numerator,denominator);
+  //分子分母都除以最大公约数
+  val n=numerator/g
+  val d=denominator/g
   /**
     * 重写java.lang.Object类中的toString方法
     * @return
     */
-  override def toString:String=numerator+"/"+denominator
+
+  //define auxiliary constructor
+  /**
+    * In Scala, every auxiliary constructor must invoke
+    * another constructor of the same class as its first action
+    */
+  def this(numerator:Int)=this(numerator,1)
+  //precondition
+  //检验参数是否合法：可以用Predef中的require方法
+  //A precondition is a con- straint on values passed into
+  // a method or constructor, a requirement which callers must fulfill.
+  require(denominator !=0,"denominator cannot equal to zero")
+  override def toString:String=n+"/"+d
+  //两个分数相加
+  def +(that:Rational):Rational=new Rational(n*that.d+that.n*d,d*that.d)
+  //两个分数相减
+  def -(that:Rational):Rational=new Rational(n*that.d - that.n*d,d*that.d)
+
+  def <(that:Rational):Boolean=n*that.d - that.n*d<0
+
+  def >(that:Rational):Boolean=n*that.d - that.n*d>0
+  def >=(that:Rational):Boolean=n*that.d - that.n*d>=0
+  def <=(that:Rational):Boolean=n*that.d - that.n*d<=0
+
+  def max(that:Rational):Rational=if (>=(that)) this else that
+
+  def min(that:Rational):Rational=if(<=(that)) this else that
+
+
+   def greatestCommonDivisor(a:Int,b:Int):Int=if (b==0) a else greatestCommonDivisor(b,a%b)
+
 
 }
--- src/com/eagle/functionalobjects/RationalNumberTest.scala
+++ src/com/eagle/functionalobjects/RationalNumberTest.scala
@@ -10,6 +10,24 @@ object RationalNumberTest {
       */
     val oneHalf=new Rational(1,2)
     println(oneHalf)
+    val invalidRational=new Rational(1,1);
+    println(invalidRational)
+
+    println(new Rational(1,2).+(new Rational(2,4)))
+    println(new Rational(1,2) + new Rational(2,4))
+
+    println(new Rational(2,3)- new Rational(1,3))
+
+    println(new Rational(2,3) min new Rational(1,3))
+    println(new Rational(2,3) max new Rational(1,3))
+
+    //invoke auxiliary  constructor:
+    println(3,1)
+
+    println(6.%(2))
+    val oneFourth=new Rational(1,4)
+    println(oneFourth.greatestCommonDivisor(8,12))
+
   }
 
 }
