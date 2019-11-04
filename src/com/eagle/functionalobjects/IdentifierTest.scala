eae53b2f524e9f1b4caec2bbde1db73204a681e9
qinlinsen <179880760@qq.com>
2019-11-04 19:34:51 +0800

    scala  identifier and functional programming in scala's book  ！

---
+++ src/com/eagle/functionalobjects/IdentifierTest.scala
@@ -0,0 +1,15 @@
+package com.eagle.functionalobjects
+
+object IdentifierTest {
+
+  def main(args: Array[String]): Unit = {
+    //如果字符后面直接加underscore 那么这个变量必须和冒号之间有个空格，否则会编译报错
+    val name_ :Int=1
+
+    //A literal identifier is an arbitrary string enclosed in back ticks (` . . . `). Some examples of literal identifiers are:
+     // `x` `<clinit>` `yield`
+    println()
+
+  }
+
+}
